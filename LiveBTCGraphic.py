61ba02f83dba5fa4ad2565bc03bd0a798d764e29
Mahmut Can Gönül <75094927+MahmutCanGonul@users.noreply.github.com>
2021-10-13 14:00:11 +0300

    Update LiveBTCGraphic.py

--- LiveBTCGraphic.py
+++ LiveBTCGraphic.py
@@ -1,6 +1,5 @@
-Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import cryptocompare
+
+import cryptocompare
 import matplotlib.pyplot as plt
 from datetime import datetime
 from matplotlib.animation import FuncAnimation
